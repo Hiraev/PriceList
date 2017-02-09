3ad51080ca8a3efb5e6871fea0a6ca1cc34c2159
Malik Hiraev <hiraev@users.noreply.github.com>
2017-02-09 19:07:28 +0300

    Project setup

---
+++ src/PriceList.java
@@ -0,0 +1,11 @@
+/**
+ * ---------------------------------------
+ * Created by Hiraev Malik on 09.02.17.
+ * GitHub: https://github.com/Hiraev
+ * vk.com: https://vk.com/hiraev
+ * ---------------------------------------
+ * All rights reserved
+ * ---------------------------------------
+ */
+public class PriceList {
+}
---
+++ tests/Test.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * ---------------------------------------
+ * Created by Hiraev Malik on 09.02.17.
+ * GitHub: https://github.com/Hiraev
+ * vk.com: https://vk.com/hiraev
+ * ---------------------------------------
+ * All rights reserved
+ * ---------------------------------------
+ */
+
+class Tests {
+    @Test
+    void foo(){
+    }
+}
