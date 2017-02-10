ffc51b7386831cd29e9cc81ca7c6d271cafa02b5
Malik Hiraev <hiraev@users.noreply.github.com>
2017-02-10 20:18:02 +0300

    Fix JUnit and "PriceList"

--- src/PriceList.java
+++ src/PriceList.java
@@ -1,7 +1,6 @@
 import javafx.util.Pair;
 
-import java.util.List;
-import java.util.Map;
+import java.util.ArrayList;
 import java.util.TreeMap;
 
 /**
@@ -13,70 +12,113 @@ import java.util.TreeMap;
  * All rights reserved
  * ---------------------------------------
  */
+
 public class PriceList {
-    private Map productMap = new TreeMap<Long, Product>();
-    private long id;
+    private TreeMap productMap = new TreeMap<Long, Product>();
 
-    public void add(Product product) {
-        productMap.put(id, product);
-        id++;
+    public int add(long id, String name, double price) {
+        if (productMap.containsKey(id) | price < 0) return -1;
+        productMap.put(id, new Product(name, price));
+        return 0;
     }
 
-    public void add(List<Product> products) {
-        for (Product product : products) {
-            productMap.put(id, product);
-            id++;
+    public int add(long id, String name) {
+        if (productMap.containsKey(id)) return -1;
+        productMap.put(id, new Product(name));
+        return 0;
+    }
+
+    public String getProduct(long id) {
+        if (!productMap.containsKey(id)) return "Error";
+        return productMap.get(id).toString();
+    }
+
+    public String getProductName(long id) {
+        if (productMap.containsKey(id)) return ((Product) productMap.get(id)).getName();
+        return "Error";
+    }
+
+    public double getProductPrice(long id) {
+        if (productMap.containsKey(id)) return ((Product) productMap.get(id)).getPrice();
+        return -1;
+    }
+
+    public int setProductName(long id, String name) {
+        if (productMap.containsKey(id)) {
+            ((Product) productMap.get(id)).setName(name);
+            return 0;
         }
+        return -1;
     }
 
-    public Product get(int id) {
-        return (Product) productMap.get(id);
-    }
-
-    public void remove(int key) {
-        productMap.remove(key);
-    }
-
-    public double calculateThePrice(List<Pair<Integer, Integer>> list) {
-        double price = 0.0;
-        double itPrice;
-        for (Pair pair : list) {
-            itPrice = ((Product) productMap.get(pair.getKey())).getPrice() * (Double) pair.getKey();
-            price += itPrice;
+    public int setProductPrice(long id, double price) {
+        if (productMap.containsKey(id) & price > 0) {
+            ((Product) productMap.get(id)).setPrice(price);
+            return 0;
         }
-        return price;
+        return -1;
+    }
+
+    public int removeProduct(long id) {
+        if (productMap.containsKey(id)) {
+            productMap.remove(id);
+            return 0;
+        }
+        return -1;
+    }
+
+    /*
+        public double calculate(Pair<Long, Integer>... list) {
+            double price = 0;
+            for (Pair product : list) {
+                //Почему следующая строчка не компилируется?
+                long id = product.getKey();
+                int quantity = (Integer) product.getValue();
+                if (productMap.containsKey(product.getKey())) {
+                    price += ((Product) productMap.get(product.getKey())).getPrice() * (Integer) product.getValue();
+                }
+            }
+            return price;
+        }
+    */
+    //Вспомогательный класс
+    private class Product {
+        private double price;
+        private String name;
+
+        Product(String name) {
+            this.name = name;
+        }
+
+        Product(String name, double price) {
+            if (price > 0.0) {
+                this.price = price;
+            }
+            this.name = name;
+        }
+
+        double getPrice() {
+            return price;
+        }
+
+        void setPrice(double price) {
+            if (price > 0.0) {
+                this.price = price;
+            }
+        }
+
+        String getName() {
+            return name;
+        }
+
+        void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return this.name + " - " + this.price;
+        }
     }
 }
 
-class Product {
-    private double price;
-    private String name;
-
-    public Product() {
-    }
-
-    public Product(String name, double price) {
-        if (price > 0.0) {
-            this.price = price;
-        }
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        if (price > 0.0) {
-            this.price = price;
-        }
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
--- test/Tests.java
+++ test/Tests.java
@@ -1,6 +1,9 @@
+import javafx.util.Pair;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 
 /**
@@ -12,24 +15,115 @@ import static org.junit.Assert.assertEquals;
  * All rights reserved
  * ---------------------------------------
  */
-
 public class Tests {
+
     PriceList priceList = new PriceList();
-    Product sausage = new Product("Колбаса", 330.40);
-    Product carrot = new Product("Морковь", 20.80);
-    Product cheese = new Product("Сыр", 450.40);
-    Product bread = new Product("Хлеб", 50.55);
-    Product cereals = new Product("Гречневые хлопья", 165.44);
+    //Покупки
+    Pair firstPurchase = new Pair(1001, 2);
+    Pair secondPurchase = new Pair(1003, 5);
+    Pair thirdPurchase = new Pair(1003, 5);
+
+
+    //Продукты
+    String productNameOne = "Торт Прага 860г.";
+    String productNameTwo = "Хлебцы Dr.Korner 7-злаков 100г.";
+    String productNameThree = "Булочка с маком";
+    String productNameFour = "Булочка веснушка";
+    String productNameFive = "Сдоба \"Ромашка\"";
+    String productNameSix = "Беляш";
+    String productNameSeven = "Плюшка московская";
+
+    double priceOne = 399.99;
+    double priceTwo = 49.90;
+    double priceThree = 15.50;
+    double priceFour = 19.00;
+    double priceFive = 20.00;
+    double priceSix = 19.80;
+
+
+    @Before
+    public void setup() {
+        priceList.add(1001, productNameOne, priceOne);
+        priceList.add(1002, productNameTwo, priceTwo);
+        priceList.add(1003, productNameThree, priceThree);
+        priceList.add(1004, productNameFour, priceFour);
+        priceList.add(1005, productNameFive);
+        priceList.add(1006, productNameSix);
+        priceList.add(1007, productNameSeven);
+    }
 
     @Test
-    public void Test() {
-        priceList.add(sausage);
-        priceList.add(carrot);
-        priceList.add(cheese);
-        priceList.add(bread);
-        priceList.add(cereals);
-        System.out.println(sausage.getName() + "dd");
-        System.out.println(((Product) priceList.get(1)).getName());
-        assertEquals(sausage, priceList.get(1));
+    public void addToPriceList() {
+        //При успешном добавлении получаем нолик
+        assertEquals(0, priceList.add(1011, productNameOne, 399.99));
+        assertEquals(0, priceList.add(1022, productNameTwo, 49.90));
+        assertEquals(0, priceList.add(1033, productNameThree, 15.50));
+        assertEquals(0, priceList.add(1044, productNameFour, 19.00));
+        //Случай, когда Id уже занят каким-то продуктом
+        assertEquals(-1, priceList.add(1004, "Этот товар не добавится", 0.00));
     }
+
+    @Test
+    public void addToPriceListWithoutPrice() {
+        assertEquals(0, priceList.add(1009, "Безымяннй товар"));
+        assertEquals(-1, priceList.add(1007, productNameSeven));
+    }
+
+    @Test
+    public void getProductName() {
+        assertEquals(productNameOne, priceList.getProductName(1001));
+        assertEquals(productNameTwo, priceList.getProductName(1002));
+        assertEquals(productNameThree, priceList.getProductName(1003));
+        assertEquals(productNameFour, priceList.getProductName(1004));
+        assertEquals("Error", priceList.getProductName(123));
+    }
+
+    @Test
+    public void getProductPrice() {
+        assertEquals(priceOne, priceList.getProductPrice(1001), 1e-3);
+        assertEquals(priceTwo, priceList.getProductPrice(1002), 1e-3);
+        assertEquals(priceThree, priceList.getProductPrice(1003), 1e-3);
+        assertEquals(priceFour, priceList.getProductPrice(1004), 1e-3);
+        assertEquals(-1, priceList.getProductPrice(4533), 1e-3);
+    }
+
+    @Test
+    public void setProductPrice() {
+        assertEquals(0, priceList.setProductPrice(1005, priceFive));
+        assertEquals(priceFive, priceList.getProductPrice(1005), 1e-3);
+        assertEquals(0, priceList.setProductPrice(1006, priceSix));
+        assertEquals(priceSix, priceList.getProductPrice(1006), 1e-3);
+        assertEquals(-1, priceList.setProductPrice(1007, -19));
+        assertEquals(-1, priceList.setProductPrice(1999, 0));
+    }
+
+    @Test
+    public void setProductName() {
+        assertEquals(0, priceList.setProductName(1001, productNameFour));
+        assertEquals(productNameFour, priceList.getProductName(1001));
+        assertEquals(-1, priceList.setProductName(9999, "Такого id там нет"));
+    }
+
+    @Test
+    public void removeProduct() {
+        //Проверяем наличие продукта
+        assertEquals(productNameSeven, priceList.getProductName(1007));
+        //Удаляем этот продукт
+        assertEquals(0, priceList.removeProduct(1007));
+        //Снова проверяем его наличие путем запроса имени
+        assertEquals("Error", priceList.getProductName(1007));
+        assertEquals(-1, priceList.removeProduct(74892));
+    }
+/*
+    @Test
+    public void calculate() {
+        //Сумма покупок
+        double sum1 = priceOne*2+priceTwo*5+priceThree*5;
+        double sum2 = priceOne*2+priceTwo*5;
+        double sum3 = priceTwo*5;
+        assertEquals(sum1,priceList.calculate(firstPurchase,secondPurchase,thirdPurchase), 1e-3);
+        assertEquals(sum2,priceList.calculate(firstPurchase,secondPurchase), 1e-3);
+        assertEquals(sum3,priceList.calculate(secondPurchase), 1e-3);
+    }
+    */
 }
